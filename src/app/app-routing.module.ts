24245eb172124bbcebe122171b05f6dba3b9aced
Thomas GRUEL <gruel@emse.fr>
2021-04-28 23:35:43 +0200

    pushing prod filder for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+// Import custom modules
+import { HomeComponent} from './home/home.component';
+import { RecordComponent} from './record/record.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'record',
+    component: RecordComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
