6455af3a93c09438046f4e33390619f9c72f6e4c
Thomas GRUEL <gruel@emse.fr>
2021-05-01 16:30:22 +0200

    add listing ngfor/ngif

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice',power:'Super Nice' },
+  { id: 12, name: 'Narco',power :'Cheap drugs' },
+  { id: 13, name: 'Bombasto',power: 'Killer farts !' },
+  { id: 14, name: 'Celeritas',power : 'Infinite celery' },
+  { id: 15, name: 'Magneta',power : "mispelled magenta !" },
+  { id: 16, name: 'RubberMan',power : 'maximum adhesion' },
+  { id: 17, name: 'Dynama',power:"Light on his bike" },
+  { id: 18, name: 'Dr IQ',power :'Thinks twice !' },
+  { id: 19, name: 'Magma',power : 'vapozize water' },
+  { id: 20, name: 'Tornado',power:'can hoover th carpet' }
+];
