a550a6f4af6cf4a432df36f08044503c4858176f
Thomas GRUEL <gruel@emse.fr>
2021-05-01 12:44:46 +0200

    add route intercepting

--- src/app/heroes/viewhero/viewhero.component.ts
+++ src/app/heroes/viewhero/viewhero.component.ts
@@ -8,9 +8,16 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ViewheroComponent implements OnInit {
 
+  heroid= 0;
 
   constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe( params => console.log(params) );
+    this.route.params.subscribe(
+      params => {
+        console.log(params),
+          this.heroid= params.id
+      }
+    );
+
   }
   ngOnInit(): void {
   }
